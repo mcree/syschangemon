45696dff444932ed427e751d8e819d2b945bba5d
Rigó Ernő <rigo.erno@sztaki.mta.hu>
2016-03-11 21:11:38 +0100

    model improvements

--- SysChangeMon/core/model.py
+++ SysChangeMon/core/model.py
@@ -1,6 +1,6 @@
 """ System Change Data Model Classes """
-
 from datetime import datetime, date
+from urllib.parse import urlunsplit, urlparse
 from uuid import uuid4
 from decimal import Decimal
 from peewee import BlobField, OperationalError
@@ -46,16 +46,76 @@ class MyTable(Table):
             return self.insert(**data)
 
 
+class State(dict):
+
+    @staticmethod
+    def from_dict(model, d: dict):
+        res = State(model, None)
+        for k, v in d.items():
+            res[k] = v
+        return res
+
+    def __init__(self, model, sessionid, url=None, **kwargs):
+        if url is None:
+            url = 'uuid://' + str(uuid4())
+        self._model = model
+        self['sessionid'] = sessionid
+        self['url'] = url
+        for k, v in kwargs:
+            self[k] = v
+
+    def save(self):
+        if 'sessionid' not in self or self['sessionid'] is None:
+            raise ValueError("value for key 'session_id' is required to save state")
+        url = urlparse(self['url'])
+        if len(url.scheme) == 0:
+            raise ValueError("value for key 'url' must have valid format, eg: scheme://path")
+        return self._model.states.upsert(columns=['url', 'sessionid'], **self)
+
+
+class Session(dict):
+
+    @staticmethod
+    def from_dict(model, d: dict):
+        res = Session(model)
+        for k, v in d.items():
+            res[k] = v
+        return res
+
+    def __init__(self, model, uuid=str(uuid4()), stamp=datetime.now(), closed=False, **kwargs):
+        self._model = model
+        self['uuid'] = uuid
+        self['stamp'] = stamp
+        self['closed'] = closed
+        for k, v in kwargs:
+            self[k] = v
+
+    def save(self):
+        return self._model.sessions.upsert(columns=['uuid'], **self)
+
+    def new_state(self, **kwargs) -> State:
+        return State(self._model, self['uuid'], **kwargs)
+
+    def get_state(self, url):
+        res = self._model.states.find_one(url=url, sessionid=self['uuid'])
+        if res is not None:
+            return State.from_dict(self._model, res)
+        else:
+            raise KeyError("state with url:"+url+" for session:"+self['uuid']+" not found")
+
+
 class Model:
     def __init__(self, db_uri="sqlite:///:memory:"):
-        self._db = MyDataSet(db_uri)
-        self._sessions = self._db['sessions']
+        self.db = MyDataSet(db_uri)
+        self.sessions = self.db['sessions']
+        self.states = self.db['states']
 
-    def create_session(self, uuid=str(uuid4()), stamp=datetime.now()):
-        session = {'uuid': uuid, 'stamp': stamp}
-        self._sessions.insert(**session)
-        return session
+    def new_session(self, uuid=str(uuid4()), stamp=datetime.now(), closed=False, **kwargs) -> Session:
+        return Session(self, uuid, stamp, closed, **kwargs)
 
-    def last_session(self):
-        uuid = self._db.query('select uuid from sessions order by stamp desc').fetchone()
-        return self._sessions.find_one(uuid = uuid)
+    def last_session(self) -> Session:
+        uuid = self.query('select uuid from sessions order by stamp desc').fetchone()
+        return Session.from_dict(self, self.sessions.find_one(uuid=uuid))
+
+    def query(self, sql, params=None, commit=None):
+        return self.db.query(sql, params, commit)
--- tests/core/test_model.py
+++ tests/core/test_model.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from core.model import Model
+from core.model import Model, Session
 
 
 class TestModel(TestCase):
@@ -7,12 +7,36 @@ class TestModel(TestCase):
         self.model = Model('sqlite:///:memory:')
 
     def dump_db(self):
-        for line in self.model._db._database.get_conn().iterdump():
+        for line in self.model.db._database.get_conn().iterdump():
             print(line)
 
-    def test_set(self):
-        print('begin test_set')
-        a = self.model.create_session(uuid='a')
-        b = self.model.create_session(uuid='b')
+    def test_last_session(self):
+        print('begin test_last_session')
+        a = self.model.new_session(uuid='a')
+        a['custom_a'] = 'val_a'
+        a.save()
+        b = self.model.new_session(uuid='b')
+        b['custom_b'] = 'val_b'
+        b.save()
         c = self.model.last_session()
+        #self.dump_db()
         self.assertEqual('b', c['uuid'])
+        self.assertEqual('val_b', c['custom_b'])
+        self.assertIsNone(c['custom_a'])
+
+    def test_state(self):
+        print("begin test_state")
+        sess = self.model.new_session()
+        a = sess.new_state(url='testurl://testurl')
+        a['key_a'] = 'val_a'
+        sess.save()
+        a.save()
+        self.assertEqual('val_a', sess.get_state(a['url'])['key_a'])
+        with self.assertRaises(KeyError):
+            sess.get_state('nonexistent://url')
+        with self.assertRaises(ValueError):
+            b = sess.new_state()
+            b['sessionid'] = None
+            b.save()
+        with self.assertRaises(ValueError):
+            sess.new_state(url='non url').save()
