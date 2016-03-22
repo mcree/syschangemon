1db7bfa5fcea760d957b5e28bfaf252dd8604d6a
Rigó Ernő <rigo.erno@sztaki.mta.hu>
2016-03-22 19:52:22 +0100

    added mock logger

--- syschangemon/utils/test.py
+++ syschangemon/utils/test.py
@@ -1,4 +1,6 @@
 """Testing utilities for syschangemon."""
+from cement.core import log
+from cement.core.log import CementLogHandler
 from cement.ext.ext_configparser import ConfigParserConfigHandler
 
 from syschangemon.cli.main import SysChangeMonTestApp, TestConfigHandler
@@ -11,6 +13,67 @@ class TestStorage:
     db = Model('sqlite:///:memory:')
 
 
+class TestLogHandler:
+    class Meta:
+        interface = log.ILog
+        label = 'test_log_handler'
+
+    def _setup(self, app_obj):
+        pass
+
+    def set_level(self):
+        pass
+
+    def get_level(self):
+        """Return a string representation of the log level."""
+        return "DEBUG"
+
+    def info(self, msg):
+        """
+        Log to the 'INFO' facility.
+
+        :param msg: The message to log.
+
+        """
+        print(msg)
+
+    def warn(self, msg):
+        """
+        Log to the 'WARN' facility.
+
+        :param msg: The message to log.
+
+        """
+        print(msg)
+
+    def error(self, msg):
+        """
+        Log to the 'ERROR' facility.
+
+        :param msg: The message to log.
+
+        """
+        print(msg)
+
+    def fatal(self, msg):
+        """
+        Log to the 'FATAL' facility.
+
+        :param msg: The message to log.
+
+        """
+        print(msg)
+
+    def debug(self, msg):
+        """
+        Log to the 'DEBUG' facility.
+
+        :param msg: The message to log.
+
+        """
+        print(msg)
+
+
 class SysChangeMonTestCase(CementTestCase):
     app_class = SysChangeMonTestApp
 
@@ -19,6 +82,7 @@ class SysChangeMonTestCase(CementTestCase):
         super(SysChangeMonTestCase, self).setUp()
         self.app.storage = TestStorage()
         self.app.config = TestConfigHandler()
+        self.app.log = TestLogHandler()
 
     def tearDown(self):
         """Override teardown actions (for every test)."""
