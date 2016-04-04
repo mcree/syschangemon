79263cdc0b0c3b2926839d7c1c7da643436a04cb
Rigó Ernő <rigo.erno@sztaki.mta.hu>
2016-04-04 23:31:54 +0200

    try to fix build

---
+++ install-fpm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+apt-get install -y curl build-essential rpm
+rm -rf ~/.gnupg
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -L https://get.rvm.io | bash -s stable
+source /etc/profile.d/rvm.sh
+rvm install ruby
+rvm use ruby --default
+rvm rubygems current
+gem install fpm
