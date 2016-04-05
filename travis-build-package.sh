e2b357f89ef459fef3e39a17f1c33ccf98fe05e4
Rigó Ernő <rigo.erno@sztaki.mta.hu>
2016-04-05 21:37:23 +0200

    refactor build process

--- travis-build-package.sh
+++ travis-build-package.sh
@@ -4,9 +4,7 @@ set -e
 
 [ "$TRAVIS_PYTHON_VERSION" != "3.4" ] && exit
 
-apt-get install rpm
-gem install fpm
-gem install package_cloud
+apt-get -y install rpm
 make
 package_cloud push mcree/syschangemon/debian/wheezy *.deb
 package_cloud push mcree/syschangemon/el/6 *.rpm
